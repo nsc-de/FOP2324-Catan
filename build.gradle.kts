a9774d4f62910fda641d4afd2bf96289a1462c7b
Nicolas Schmidt <nicolas@schmidtworld.net>
2024-02-13 16:52:51 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,9 @@ submission {
     // ACHTUNG!
     // Setzen Sie im folgenden Bereich Ihre TU-ID (NICHT Ihre Matrikelnummer!), Ihren Nachnamen und Ihren Vornamen
     // in Anführungszeichen (z.B. "ab12cdef" für Ihre TU-ID) ein!
-    studentId = null
-    firstName = null
-    lastName = null
+    studentId = "ns70seky"
+    firstName = "Nicolas"
+    lastName = "Schmidt"
 
     // Optionally require own tests for mainBuildSubmission task. Default is false
     requireTests = false
